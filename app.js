1d5357b9e99ac605742f5bef9522eef5350aa9c6
HanSangHyeok <tkdgur8377@gmail.com>
2019-02-25 21:25:39 +0900

    Finish styling (using webpack)

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
-import { localsMiddleware } from "./middleware";
+import {
+    localsMiddleware
+} from "./middleware";
 import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -13,10 +15,13 @@ const app = express();
 
 app.use(helmet());
 app.set("view engine", "pug");
-app.use("/uploads", express.static("uploads"))
+app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(morgan("dev"));
 
 app.use(localsMiddleware);
