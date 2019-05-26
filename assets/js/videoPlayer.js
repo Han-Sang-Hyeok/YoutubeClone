63930cb3b3412619d1a497baa121611ddc1aa531
HanSangHyeok <tkdgur8377@gmail.com>
2019-05-26 16:26:31 +0900

    video time set

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -3,6 +3,10 @@ const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
 const volumeBtn = document.getElementById("jsVolumeButton");
+const fullScrnBtn = document.getElementById("jsExpandButton");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
+
 
 function handlePlayClick(){
     if(videoPlayer.paused){
@@ -15,21 +19,82 @@ function handlePlayClick(){
 }
 
 function handleVolumeClick() {
-    if (videoPlayer.muted) {
-      videoPlayer.muted = false;
-      volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
-    } else {
-      videoPlayer.muted = true;
-      volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
-    }
+  if (videoPlayer.muted) {
+    videoPlayer.muted = false;
+    volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+  } else {
+    videoPlayer.muted = true;
+    volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
   }
+}
+
+function goSmallScreen(){
+  fullScrnBtn.innerHTML = '<i class = "fas fa-expand"></i>';
+  fullScrnBtn.removeEventListener("click", goSmallScreen);
+  fullScrnBtn.addEventListener("click", goFullScreen);
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
+}
+
+function goFullScreen(){
+  fullScrnBtn.innerHTML = '<i class="fas fa-compress"></i>';
+  fullScrnBtn.removeEventListener("click",goFullScreen);
+  fullScrnBtn.addEventListener("click", goSmallScreen);
+  if (videoContainer.requestFullscreen) {
+    videoContainer.requestFullscreen();
+  } else if (videoContainer.mozRequestFullScreen) {
+    videoContainer.mozRequestFullScreen();
+  } else if (videoContainer.webkitRequestFullscreen) {
+    videoContainer.webkitRequestFullscreen();
+  } else if (videoContainer.msRequestFullscreen) {
+    videoContainer.msRequestFullscreen();
+  }
+}
+
+const formatDate = seconds => {
+  const secondsNumber = parseInt(seconds, 10);
+  let hours = Math.floor(secondsNumber / 3600);
+  let minutes = Math.floor((secondsNumber - hours * 3600) / 60);
+  let totalSeconds = secondsNumber - hours * 3600 - minutes * 60;
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (seconds < 10) {
+    totalSeconds = `0${totalSeconds}`;
+  }
+  return `${hours}:${minutes}:${totalSeconds}`;
+};
+
+//현재 시간 설정
+function getCurrentTime() {
+  currentTime.innerHTML = formatDate(videoPlayer.currentTime);
+}
+
+function setTotalTime() {
+  const totalTimeString = formatDate(videoPlayer.duration);
+  totalTime.innerHTML = totalTimeString;
+  //1초마다 현재시간 설정하기 위해서 setInterval사용!
+  setInterval(getCurrentTime, 1000);
+}
+
 
 function init(){
 //    videoPlayer = videoContainer.querySelector("video");
-    playBtn.addEventListener(
-        "click", handlePlayClick
-    );
+    playBtn.addEventListener("click", handlePlayClick);
     volumeBtn.addEventListener("click", handleVolumeClick);
+    fullScrnBtn.addEventListener("click", goFullScreen);
+    videoPlayer.addEventListener("loadedmetadata", setTotalTime);
 }
 
 if(videoContainer){
